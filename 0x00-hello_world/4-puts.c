3b384f6a54e6c8cbd0475c1a42f266e7f7bb8eb9
Abdulai Halidu <abdulaihalidu008@gmail.com>
2022-09-08 10:39:42 -0700

    Write to the standard output using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Program entry point
  *
- * Return - 0 is successful
+ * Return: 0 if successful
  */
 int main(void) 
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return  (0);
 }
