8bc1f437a810edf22e7d663455018c12327bb792
Abdulai Halidu <abdulaihalidu008@gmail.com>
2022-09-09 13:31:27 -0700

    Added more solutions

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = (hundreds + 1); tens <= '9'; tens++)
+		{
+			for (ones = (tens + 1); ones <= '9'; ones++)
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
+
+				if (hundreds != '7' || tens != '8' || ones != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+}
