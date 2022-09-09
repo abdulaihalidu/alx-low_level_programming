8d2ec0f270aab3644b893124ab4d7e8b0749510d
Abdulai Halidu <abdulaihalidu008@gmail.com>
2022-09-09 12:43:55 -0700

    Added more solutions

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+	int num = '0';
+	int base_16_a_f = 'a';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	while (base_16_a_f <= 'f')
+	{
+		putchar(base_16_a_f)
+			base_16_a_f++;
+	}
+	putchar('\n');
+}
