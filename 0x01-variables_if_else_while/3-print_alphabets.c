b51e850eccd698d05d6ed346aa077626509ea51c
Abdulai Halidu <abdulaihalidu008@gmail.com>
2022-09-09 11:15:39 -0700

    Added more solutions

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'A';
+
+	while (lower <= 'z')
+	{
+		putchar(letter);
+		lower++;
+	}
+	putchar('\n');
+
+
+	while (upper <= 'Z')
+	{
+		putchar(upper);
+		upper++;
+	}
+	putchar('\n');
+}
